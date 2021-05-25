3bbcd8382d29e1724ca3a2dad0e9f6ec4dd86ff8
Takeyuki UEDA <ueda@latelierdueda.com>
2021-05-25 19:35:59 +0900

    first version

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,34 +3,32 @@
     <v-toolbar>
       <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <slot name="title"></slot>
-      <!--<v-toolbar-title class="grey--text">
-        <span class="font-weight-light">Site</span>
-        <span>Title</span>
-      </v-toolbar-title> -->
-      <v-spacer></v-spacer>
+      <slot name="title">
+        <v-toolbar-title class="grey--text">
+          <span class="font-weight-light">{{titleStr}}</span>
+          <span>Title</span>
+        </v-toolbar-title>
+      </slot>
+      <slot name="menu">
+        <v-spacer></v-spacer>
 
-      <!-- dropdown menu -->
-      <v-menu offset-y>
-        <template v-slot:activator="{ on }">
-        <!-- <v-btn text slot="activator"> -->
-        <v-btn text v-on="on">
-          <v-icon left>expand_more</v-icon>
-          <span>Menu</span>
-        </v-btn>
-        </template>
-        <v-list>
-          <!-- v-list-tile is changed to v-list-item -->
-          <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
-            <v-list-item-title>{{ link.text }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-
-      <v-btn text>
-        <span>Sign Out</span>
-        <v-icon right>exit_to_app</v-icon>
-      </v-btn>
+        <!-- dropdown menu -->
+        <v-menu offset-y>
+          <template v-slot:activator="{ on }">
+          <!-- <v-btn text slot="activator"> -->
+          <v-btn text v-on="on">
+            <v-icon left>expand_more</v-icon>
+            <span>Menu</span>
+          </v-btn>
+          </template>
+          <v-list>
+            <!-- v-list-tile is changed to v-list-item -->
+            <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+              <v-list-item-title>{{ link.text }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </slot>
     </v-toolbar>
 
     <v-navigation-drawer v-model="drawer" app class="primary">
@@ -53,16 +51,20 @@
 
 <script>
 import {QRcode} from 'qrcode-of-this-site'
-import {AccountRoutes, PurchaseRoutes} from 'vue-faui-user-fe/'
+//import {AccountRoutes, PurchaseRoutes} from 'vue-faui-user-fe/'
 export default {
+  props: {
+    titleStr: String,
+    links: Array,
+  },
   components: {QRcode},
   data: () => ({
     drawer: false,
-    links: [
+/*    links: [
       { icon: 'home', text: 'Home', route: '/'},
       AccountRoutes.menuItem,
       PurchaseRoutes.menuItem,
-    ]
+    ]*/
   }),
 }
 </script>
