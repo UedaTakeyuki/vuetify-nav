9d890510daa8adc491d2416158fcdde1547ffccf
Takeyuki UEDA <ueda@latelierdueda.com>
2021-05-27 10:20:50 +0900

    v1.0.4

--- src/components/updateLogin.sh
+++
@@ -1,4 +0,0 @@
-rm Login.vue EmailVerificationRequest.vue
-
-svn export https://github.com/UedaTakeyuki/vue-faui-user-fe/trunk/src/components/Login.vue
-svn export https://github.com/UedaTakeyuki/vue-faui-user-fe/trunk/src/components/EmailVerificationRequest.vue
