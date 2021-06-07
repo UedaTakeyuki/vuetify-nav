966caaa1a423282892ddf1f7d25c9ea2caf58a36
Takeyuki UEDA <ueda@latelierdueda.com>
2021-06-07 09:11:05 +0900

    no dependencies

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,8 @@ module.exports = {
 // https://stackoverflow.com/questions/61452230/mixed-up-css-on-production-build-of-vue-cli
   css:{
     extract:false  
+  },
+  configureWebpack: {
+    entry: './src/build-entry.js', 
   }
 }
