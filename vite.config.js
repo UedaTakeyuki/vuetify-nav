f915195989992d9731e4c70f64130607d81b6794
Takeyuki UEDA <ueda@latelierdueda.com>
2022-12-20 20:28:56 +0900

    Vue3 branch first version

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: '/src/components/build-entry.js',
+      name: 'vuetify-nav',
+      fileName: (format) => `vuetify-nav.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // Add external deps here
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+  plugins: [vue()],
+})
