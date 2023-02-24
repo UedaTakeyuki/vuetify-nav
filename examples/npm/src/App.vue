04fa0ecdadf21b4921ce8a0e4045717c2dadd5c5
Takeyuki UEDA <ueda@latelierdueda.com>
2023-02-24 09:59:51 +0900

    v2.0.5

---
+++ examples/npm/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-main>
+      <Navbar/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import {Navbar} from 'vuetify-nav'
+export default {
+  name: 'App',
+
+  components: {
+    Navbar
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
