619b9c75187984a9d8e9d9c65891ba45f17e7cc0
Takeyuki UEDA <ueda@latelierdueda.com>
2023-02-24 09:59:51 +0900

    v2.0.5

---
+++ examples/cdn/vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  chainWebpack: (config) => {
+    // https://qiita.com/uturned0/items/5fdf2d92548274fe56e3
+    // https://rinoguchi.net/2020/05/vue-firebase-reduce-bundle-size.html
+    // https://stackoverflow.com/questions/66179210/how-to-externalize-a-lib-with-vue-cli
+      config.externals({
+        vue: 'Vue',
+        vuetify: 'Vuetify',
+      })
+  }
+}
